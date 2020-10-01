78f46fa46c505e00e833ee05140bdf743472ded7
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-01 12:59:42 +0200

    Add build dir

---
+++ build/precache-manifest.6e79cc639c9ecab99466fb9e1f8a225a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4142903dfe558dcac0d902f2f1e4a90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d2bbbdfe6aceaf7c454",
+    "url": "/static/css/2.ecf70e4c.chunk.css"
+  },
+  {
+    "revision": "14da028bbb8135aaffbf",
+    "url": "/static/css/main.bc6d5e3d.chunk.css"
+  },
+  {
+    "revision": "6d2bbbdfe6aceaf7c454",
+    "url": "/static/js/2.9802d1b9.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/static/js/2.9802d1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14da028bbb8135aaffbf",
+    "url": "/static/js/main.611c32b1.chunk.js"
+  },
+  {
+    "revision": "1f8e174327d4d4671cc1",
+    "url": "/static/js/runtime-main.b0a6c4bb.js"
+  },
+  {
+    "revision": "afc1550a0b170efd8f270a8316c85da7",
+    "url": "/static/media/Abel-Regular.afc1550a.ttf"
+  },
+  {
+    "revision": "9bf126fdf94f5b91a94b308d9867aa93",
+    "url": "/static/media/Abract01.9bf126fd.png"
+  },
+  {
+    "revision": "10c6818802ed96f9d6976633ae148c37",
+    "url": "/static/media/Anton-Regular.10c68188.ttf"
+  },
+  {
+    "revision": "2f19f2abf570ca58bf36241ea0e5c6ba",
+    "url": "/static/media/Background-image.2f19f2ab.png"
+  },
+  {
+    "revision": "8e96daaaad1703684a9d7011f9fbe90a",
+    "url": "/static/media/JosefinSans-Regular.8e96daaa.ttf"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/static/media/LexendDeca-Regular.d90b3bff.ttf"
+  },
+  {
+    "revision": "8e18de89d104d474b86a37a37ea45dde",
+    "url": "/static/media/Livvic-Regular.8e18de89.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ab3d073e4176608590a3ddc906837e6d",
+    "url": "/static/media/post-1.ab3d073e.jpg"
+  },
+  {
+    "revision": "76f7a1f9967e558cca9706b844ca7930",
+    "url": "/static/media/post-2.76f7a1f9.jpg"
+  },
+  {
+    "revision": "09b7ee20211c445ae883bcb1136b4e61",
+    "url": "/static/media/post-3.09b7ee20.jpg"
+  }
+]);

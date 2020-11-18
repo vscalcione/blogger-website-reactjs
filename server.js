8afe838c0876d524870afb969cbe0d7af08fa96f
vscalcione <vincenzo99.scalcione@gmail.com>
2020-11-18 18:51:13 +0100

    Heroku deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 //Serve only the static files for the dist directory
-app.use(express.static(__dirname + '/dist/blogger-website-reactjs'));
+app.use(express.static(__dirname + '/build/'));
 
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/dist/blogger-webiste-reactjs/index.html'));
+	res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 //Start the app listening on the default Heroku port
